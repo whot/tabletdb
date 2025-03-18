d3b4810c789f52f615ac1decd4fbfa6ee011a173
Peter Hutterer <peter.hutterer@who-t.net>
2025-03-18 15:14:56 +1000

    cli: allow info with extra arguments

--- crates/tabletdb-cli/src/main.rs
+++ crates/tabletdb-cli/src/main.rs
@@ -4,8 +4,69 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 use std::process::ExitCode;
 use tabletdb::{
-    BusType, Button, Dial, Feature, Location, Ring, Strip, Tablet, TabletInfo, Tool, Units,
+    BusType, Button, Dial, Feature, Location, ProductId, Ring, Strip, Tablet, TabletInfo, Tool,
+    Units, VendorId,
 };
+
+#[derive(Clone, Debug)]
+struct Hex(u16);
+
+impl std::str::FromStr for Hex {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        u16::from_str_radix(s.trim_start_matches("0x"), 16)
+            .map_err(|e| format!("{e}"))
+            .map(Hex)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct DeviceMatch {
+    bustype: String,
+    vid: Hex,
+    pid: Hex,
+    name: Option<String>,
+    uniq: Option<String>,
+}
+
+impl std::str::FromStr for DeviceMatch {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let components: Vec<&str> = s.split("|").collect();
+        if components.len() < 3 {
+            return Err(format!("{s} is not a valid DeviceMatch"));
+        }
+
+        let bustype = components.first().map(|s| String::from(*s)).unwrap();
+        if ["usb", "bluetooth", "i2c", "serial"]
+            .iter()
+            .all(|bt| *bt != bustype)
+        {
+            return Err(format!("Unsupported bus type {bustype}"));
+        }
+
+        let vid = components.get(1).map(|s| Hex::from_str(s)).unwrap()?;
+        let pid = components.get(2).map(|s| Hex::from_str(s)).unwrap()?;
+        let name = components
+            .get(3)
+            .filter(|s| !s.is_empty())
+            .map(|s| String::from(*s));
+        let uniq = components
+            .get(4)
+            .filter(|s| !s.is_empty())
+            .map(|s| String::from(*s));
+
+        Ok(DeviceMatch {
+            bustype,
+            vid,
+            pid,
+            name,
+            uniq,
+        })
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -40,6 +101,34 @@ enum Commands {
         /// The path to a device
         #[arg(long)]
         device: Option<String>,
+
+        /// The bus type
+        #[arg(long,
+             value_parser = clap::builder::PossibleValuesParser::new(
+                ["usb", "bluetooth", "i2c", "serial"]
+            ),
+        )]
+        bustype: Option<String>,
+
+        /// The vendor id (overrides path and device-match)
+        #[arg(long, value_parser = clap::value_parser!(Hex))]
+        vendor_id: Option<Hex>,
+
+        /// The product id (overrides path and device-match)
+        #[arg(long, value_parser = clap::value_parser!(Hex))]
+        product_id: Option<Hex>,
+
+        /// The kernel name (overrides path and device-match)
+        #[arg(long)]
+        kernel_name: Option<String>,
+
+        /// The uniq value (overrides path and device-match)
+        #[arg(long)]
+        uniq: Option<String>,
+
+        /// A libwacom data-file compatible match
+        #[arg(long, value_parser = clap::value_parser!(DeviceMatch))]
+        device_match: Option<DeviceMatch>,
     },
 }
 
@@ -93,113 +182,166 @@ fn cmd_list_styli() -> Result<()> {
     Ok(())
 }
 
-fn cmd_info(path: Option<String>) -> Result<()> {
-    if let Some(path) = path {
-        let cache = tabletdb::Cache::new()?;
-        let info = TabletInfo::new_from_path(&PathBuf::from(&path))?;
-        let mut tablets = cache.tablets().filter(|t| *t == &info).peekable();
-        if tablets.next().is_none() {
-            println!("{:?} is not a known tablet device", path);
-            return Ok(());
-        }
-        tablets.for_each(|tablet| {
-            println!("device:");
-            println!("  name: \"{}\"", tablet.name());
-            println!("  model_name: \"{}\"", tablet.model_name().unwrap_or(""));
-            println!("  kernel_name: \"{}\"", tablet.kernel_name().unwrap_or(""));
-            println!(
-                "  firmware_version: \"{}\"",
-                tablet.firmware_version().unwrap_or("")
-            );
-            println!(
-                "  layout: \"{}\"",
-                tablet
-                    .layout()
-                    .map(|l| String::from(l.to_string_lossy()))
-                    .unwrap_or(String::from(""))
-            );
-            println!(
-                "  bus: \"{}\"",
-                match tablet.bustype() {
-                    BusType::USB => "usb",
-                    BusType::Bluetooth => "bluetooth",
-                    BusType::Serial => "serial",
-                    BusType::I2C => "i2c",
-                }
-            );
-            println!("  vid: \"0x{:04x}\"", tablet.vendor_id());
-            println!("  pid: \"0x{:04x}\"", tablet.product_id());
-            println!("  width: {}", tablet.width().inches());
-            println!("  height: {}", tablet.height().inches());
-            println!("  reversible: {}", tablet.is_reversible());
-            println!("  stylus: {}", tablet.supports_stylus());
-            println!("  touch: {}", tablet.supports_touch());
-            println!("  touchswitch: {}", tablet.has_touchswitch());
+fn cmd_info(
+    path: Option<String>,
+    bustype: Option<String>,
+    vendor_id: Option<Hex>,
+    product_id: Option<Hex>,
+    kernel_name: Option<String>,
+    uniq: Option<String>,
+    device_match: Option<DeviceMatch>,
+) -> Result<()> {
+    let info: TabletInfo = if let Some(path) = &path {
+        TabletInfo::new_from_path(&PathBuf::from(path))?
+    } else {
+        TabletInfo::new()
+    };
 
-            let buttons: Vec<&Button> = tablet.buttons().collect();
-            if !buttons.is_empty() {
-                println!("  buttons:");
-                for button in buttons {
-                    println!(
-                        "    - {{ idx: {}, location: \"{}\", evdev: \"0x{:x}\" }}",
-                        button.index().number(),
-                        match button.location() {
-                            Location::Left => "left",
-                            Location::Right => "right",
-                            Location::Top => "top",
-                            Location::Bottom => "bottom",
-                        },
-                        button.evdev_code().code()
-                    );
-                }
+    let bustype = bustype.or(device_match.clone().map(|m| m.bustype));
+    let info = if let Some(bustype) = bustype {
+        let bt = match bustype.as_str() {
+            "usb" => BusType::USB,
+            "bluetooth" => BusType::Bluetooth,
+            "i2c" => BusType::I2C,
+            "serial" => BusType::Serial,
+            _ => panic!("Unsupported bus type {bustype}"),
+        };
+        info.bustype(bt)
+    } else {
+        info
+    };
+
+    let vendor_id = vendor_id.or(device_match.clone().map(|m| m.vid));
+    let info = if let Some(vendor_id) = vendor_id {
+        info.vid(VendorId::from(vendor_id.0))
+    } else {
+        info
+    };
+
+    let product_id = product_id.or(device_match.clone().map(|m| m.pid));
+    let info = if let Some(product_id) = product_id.or(device_match.clone().map(|m| m.pid)) {
+        info.pid(ProductId::from(product_id.0))
+    } else {
+        info
+    };
+
+    let kernel_name = kernel_name.or(device_match.clone().and_then(|m| m.name));
+    let info = if let Some(name) = kernel_name {
+        info.kernel_name(name)
+    } else {
+        info
+    };
+
+    let uniq = uniq.or(device_match.clone().and_then(|m| m.uniq));
+    let info = if let Some(uniq) = uniq {
+        info.uniq(uniq)
+    } else {
+        info
+    };
+
+    let cache = tabletdb::Cache::new()?;
+    let mut tablets = cache.tablets().filter(|t| *t == &info).peekable();
+    if tablets.next().is_none() {
+        println!("Not a known tablet device");
+        return Ok(());
+    }
+    for tablet in tablets {
+        println!("device:");
+        println!("  name: \"{}\"", tablet.name());
+        println!("  model_name: \"{}\"", tablet.model_name().unwrap_or(""));
+        println!("  kernel_name: \"{}\"", tablet.kernel_name().unwrap_or(""));
+        println!(
+            "  firmware_version: \"{}\"",
+            tablet.firmware_version().unwrap_or("")
+        );
+        println!(
+            "  layout: \"{}\"",
+            tablet
+                .layout()
+                .map(|l| String::from(l.to_string_lossy()))
+                .unwrap_or(String::from(""))
+        );
+        println!(
+            "  bus: \"{}\"",
+            match tablet.bustype() {
+                BusType::USB => "usb",
+                BusType::Bluetooth => "bluetooth",
+                BusType::Serial => "serial",
+                BusType::I2C => "i2c",
             }
+        );
+        println!("  vid: \"0x{:04x}\"", tablet.vendor_id());
+        println!("  pid: \"0x{:04x}\"", tablet.product_id());
+        println!("  width: {}", tablet.width().inches());
+        println!("  height: {}", tablet.height().inches());
+        println!("  reversible: {}", tablet.is_reversible());
+        println!("  stylus: {}", tablet.supports_stylus());
+        println!("  touch: {}", tablet.supports_touch());
+        println!("  touchswitch: {}", tablet.has_touchswitch());
 
-            fn print_feature(f: &impl Feature) {
+        let buttons: Vec<&Button> = tablet.buttons().collect();
+        if !buttons.is_empty() {
+            println!("  buttons:");
+            for button in buttons {
                 println!(
-                    "    - {{ idx: {}, modes: {}, has_status_led: {}, buttons: [{}] }}",
-                    f.index(),
-                    f.num_modes(),
-                    f.has_status_led(),
-                    f.buttons()
-                        .map(|idx| format!("{}", idx.number()))
-                        .collect::<Vec<String>>()
-                        .join(", ")
-                )
+                    "    - {{ idx: {}, location: \"{}\", evdev: \"0x{:x}\" }}",
+                    button.index().number(),
+                    match button.location() {
+                        Location::Left => "left",
+                        Location::Right => "right",
+                        Location::Top => "top",
+                        Location::Bottom => "bottom",
+                    },
+                    button.evdev_code().code()
+                );
             }
+        }
 
-            let rings: Vec<&Ring> = tablet.rings().collect();
-            if !rings.is_empty() {
-                println!("  rings:");
-                for ring in rings {
-                    print_feature(ring);
-                }
-            }
-            let dials: Vec<&Dial> = tablet.dials().collect();
-            if !dials.is_empty() {
-                println!("  dials:");
-                for dial in dials {
-                    print_feature(dial);
-                }
-            }
-            let strips: Vec<&Strip> = tablet.strips().collect();
-            if !strips.is_empty() {
-                println!("  strips:");
-                for strip in strips {
-                    print_feature(strip);
-                }
-            }
+        fn print_feature(f: &impl Feature) {
+            println!(
+                "    - {{ idx: {}, modes: {}, has_status_led: {}, buttons: [{}] }}",
+                f.index(),
+                f.num_modes(),
+                f.has_status_led(),
+                f.buttons()
+                    .map(|idx| format!("{}", idx.number()))
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            )
+        }
 
-            if tablet.supports_stylus() {
-                println!("  styli:");
-                for tool in tablet.tools() {
-                    println!(
-                        "    - {{ vid: '0x{:04x}', pid: '0x{:08x}' }}",
-                        tool.vendor_id(),
-                        tool.tool_id()
-                    );
-                }
+        let rings: Vec<&Ring> = tablet.rings().collect();
+        if !rings.is_empty() {
+            println!("  rings:");
+            for ring in rings {
+                print_feature(ring);
             }
-        })
+        }
+        let dials: Vec<&Dial> = tablet.dials().collect();
+        if !dials.is_empty() {
+            println!("  dials:");
+            for dial in dials {
+                print_feature(dial);
+            }
+        }
+        let strips: Vec<&Strip> = tablet.strips().collect();
+        if !strips.is_empty() {
+            println!("  strips:");
+            for strip in strips {
+                print_feature(strip);
+            }
+        }
+
+        if tablet.supports_stylus() {
+            println!("  styli:");
+            for tool in tablet.tools() {
+                println!(
+                    "    - {{ vid: '0x{:04x}', pid: '0x{:08x}' }}",
+                    tool.vendor_id(),
+                    tool.tool_id()
+                );
+            }
+        }
     }
 
     Ok(())
@@ -302,7 +444,23 @@ fn run() -> Result<()> {
         Commands::ListTablets => cmd_list_tablets(),
         Commands::ListStyli => cmd_list_styli(),
         Commands::ListLocalDevices => cmd_list_local(),
-        Commands::Info { device } => cmd_info(device),
+        Commands::Info {
+            device,
+            bustype,
+            vendor_id,
+            product_id,
+            kernel_name,
+            uniq,
+            device_match,
+        } => cmd_info(
+            device,
+            bustype,
+            vendor_id,
+            product_id,
+            kernel_name,
+            uniq,
+            device_match,
+        ),
     }
 }
 
